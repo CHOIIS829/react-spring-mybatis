ef17eed2d8d91fe19ea2b70856a5f2ac6ea19163
CHOIIS829 <cth7097@gmail.com>
2024-05-03 13:57:56 +0900

    예외처리 수정

--- demo/src/main/java/com/example/projectx/global/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/example/projectx/global/exception/GlobalExceptionHandler.java
@@ -1,15 +1,25 @@
 package com.example.projectx.global.exception;
 
+import com.example.projectx.global.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ResponseDto> IllegalArgumentException(IllegalArgumentException e){
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setSuccess(false);
+        responseDto.setMessage(e.getMessage());
+        return ResponseEntity.badRequest().body(responseDto);
     }
 }
