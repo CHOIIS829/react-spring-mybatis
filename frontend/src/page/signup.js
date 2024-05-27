e4455e8b777aac777099e5bd1c7e261a040b9ebc
tromxx <93820349+tromxx@users.noreply.github.com>
2024-05-27 17:26:58 +0900

    회원 가입 완료 모달 창 confirm close 수정 필요 및 css

--- frontend/src/page/signup.js
+++ frontend/src/page/signup.js
@@ -11,16 +11,17 @@ const SignUpContainer = styled.form`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap: 20px;
+    gap: 30px;
     border-radius: 15px;
     margin-top: 10%;
     padding: 50px;
+    width: 25%;
     h1{
         cursor: pointer;
     }
     input{
         box-sizing: border-box; 
-        width: 100%;
+        width: 80%;
         height: 45px;
         border: none;
         border-radius: 10px;
@@ -37,6 +38,10 @@ const SignUpContainer = styled.form`
         background-color: var(--main-color);
         color: white;
     }
+    p{
+        margin: 0;
+        font-weight: bolder;
+    }
 `;
 
 export const SignUp = () => {
@@ -45,7 +50,10 @@ export const SignUp = () => {
     const [children, setChildren] = useState("");
     const [email, setEmail] = useState("");
     const [confirmEmail, setConfirmEmail] = useState(false);
-    const [password, setPassword] = useState("");
+    const [pwd, setPwd] = useState("");
+    const [rePwd, setRePwd] = useState("");
+    const [confirmPwd, setConfirmPwd] = useState(false);
+    const [confirmRePwd, setConfirmRePwd] = useState(false);
 
     useEffect(()=>{
         //setChildren("약관 동의 땡땡땡");
@@ -57,21 +65,34 @@ export const SignUp = () => {
     } 
     
     const handleEmail = (e) => {
+        const emailValue = e.target.value;
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        setEmail(e.target.value);
-        if(regex.test(e.target.value)){
+        setEmail(emailValue);
+        if (regex.test(emailValue)) {
             setConfirmEmail(true);
-        }else{
+        } else {
             setConfirmEmail(false);
         }
     }
 
-    const handlePassword = (e) => {
-        const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/
-        if(regex.test(e.target.value)){
-            setPassword(e.target.value);
+    const handlePwd = (e) => {
+        const pwdValue = e.target.value;
+        const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+        setPwd(pwdValue);
+        if (regex.test(pwdValue)) {
+            setConfirmPwd(true);
+        } else {
+            setConfirmPwd(false);
+        }
+    }
+
+    const handleRePwd = (e) => {
+        const rePwdValue = e.target.value;
+        setRePwd(rePwdValue);
+        if(pwd === rePwdValue){
+            setConfirmRePwd(true);
         }else{
-            console.log(false)
+            setConfirmRePwd(false);
         }
     }
 
@@ -80,7 +101,7 @@ export const SignUp = () => {
 
         const data = {
             email: email,
-            memberPwd: password,
+            memberPwd: pwd,
             memberName: "test"
         };
         if(confirmEmail){
@@ -105,9 +126,11 @@ export const SignUp = () => {
             <SignUpContainer>
                 <h1 onClick={()=>moveTo("/")}>project<span style={{color:'#F26F23', fontSize:'35px'}}>X</span></h1>
                 <input placeholder="이메일 을 입력하세요." autoComplete="username" onChange={(e)=>handleEmail(e)}/>
-                {console.log(email.length)}
-                <input placeholder="비밀번호" type="password" autoComplete="current-password" onChange={(e)=>handlePassword(e)}/>
-                <input placeholder="비밀번호 확인" type="password" autoComplete="current-password" onChange={(e)=>handlePassword(e)}/>
+                {email.length > 0 ? confirmEmail ? <p style={{color : 'green'}}>올바른 형식입니다.</p> : <p style={{color : 'red'}}>이메일 형식으로 입력해주세요.</p> : null}
+                <input placeholder="비밀번호" type="password" autoComplete="current-password" onChange={(e)=>handlePwd(e)}/>
+                {pwd.length > 0 ? confirmPwd ? <p style={{color : 'green'}}>안정한 비밀번호에요.</p> : <p style={{color : 'red'}}>숫자, 영문자, 특수문자를 포함한 8~25자리로 입력해주세요.</p> : null}
+                <input placeholder="비밀번호 확인" type="password" autoComplete="current-password" onChange={(e)=>handleRePwd(e)}/>
+                {rePwd.length > 0 ? confirmRePwd ? <p style={{color : 'green'}}>비밀번호가 일치 합니다.</p> : <p style={{color : 'red'}}>비밀번호가 일치하지 않습니다.</p> : null}
                 <input placeholder="이름 을 입력하세요." autoComplete="name"/>
                 <button onClick={(e)=>handleSignUp(e)}>회원가입</button>
             </SignUpContainer>
