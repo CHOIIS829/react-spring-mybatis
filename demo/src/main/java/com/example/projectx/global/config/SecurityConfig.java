cd9a8e1c8c512f9ff89976e9fc593441be39d1ed
CHOIIS829 <cth7097@gmail.com>
2024-05-27 16:28:28 +0900

    cors 설정

--- demo/src/main/java/com/example/projectx/global/config/SecurityConfig.java
+++ demo/src/main/java/com/example/projectx/global/config/SecurityConfig.java
@@ -2,11 +2,15 @@ package com.example.projectx.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
         http.csrf((auth) -> auth.disable()); // csrf disable
-        http.cors((auth) -> auth.disable()); // cors disable
+        http.cors((auth) -> auth.configurationSource(corsConfigurationSource())); // cors disable
         http.formLogin((auth) -> auth.disable()); // form login disable
         http.httpBasic((auth) -> auth.disable()); // http basic disable
 
@@ -39,6 +43,25 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOriginPattern("*"); // Allow all origins (you can specify specific origins)
+        configuration.addAllowedMethod(HttpMethod.GET);
+        configuration.addAllowedMethod(HttpMethod.POST);
+        configuration.addAllowedMethod(HttpMethod.PUT);
+        configuration.addAllowedMethod(HttpMethod.DELETE);
+        configuration.addAllowedMethod(HttpMethod.OPTIONS);
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
 
 
 }
